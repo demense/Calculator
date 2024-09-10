247673d7b606fd68e3fd71db47ce5afa0161830f
Ahmed <ahmedadil9102@gmail.com>
2024-09-10 11:07:03 +0300

    Link the ui to the javascript file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Here are the functions for the basic math operations
 function add(a, b) {
   return a + b;
 }
@@ -14,21 +15,60 @@ function divide(a, b) {
   return a / b;
 }
 
-let firstNumber;
-let secondNumber;
-let operator;
+function modulus(a, b) {
+  return a % b;
+}
 
-function operate(firstNum, secondNum, ope) {
-  Number(firstNum);
-  Number(secondNum);
+// This is the function that operate on the given values
+// a -> the first number
+// b -> the second number
+// ope -> the operator
+function operate([a, ope, b]) {
   if (ope === "+") {
-    return add(firstNum, secondNum);
+    return add(a, b);
   } else if (ope === "-") {
-    return subtract(firstNum, secondNum);
-  } else if (ope === "*") {
-    return multiply(firstNum, secondNum);
+    return subtract(a, b);
+  } else if (ope === "x") {
+    return multiply(a, b);
   } else if (ope === "/") {
-    return divide(firstNum, secondNum);
+    return divide(a, b);
+  } else if (ope === "%") {
+    return modulus(a, b);
   }
 }
 
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let displayValue = [];
+let isFirstNumber = true;
+
+// Here we select the all the button that has the class ".number"
+const Buttons = document.querySelectorAll(".buttons");
+//
+Buttons.forEach((button) => {
+  button.addEventListener("click", (event, value) => {
+    if (event.target.classList.contains("number")) {
+      numberValue = event.target.value;
+      if (isFirstNumber) {
+        firstNumber += numberValue;
+      } else {
+        secondNumber += numberValue;
+      }
+    }
+
+    if (event.target.classList.contains("function")) {
+      functionValue = event.target.value;
+      operator = functionValue;
+      isFirstNumber = false;
+    }
+
+    displayValue = [firstNumber,operator,secondNumber];
+
+    if (event.target.id === "equal") {
+      console.log(operate(displayValue));
+    }
+  });
+});
+
+console.log();
